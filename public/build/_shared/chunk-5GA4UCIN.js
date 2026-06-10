6bd3dd252d5b0610ab453dabc7cecb5a27cd1f34
wn <45404661+ob-xyz@users.noreply.github.com>
2026-06-10 08:40:38 -0400

    11

---
+++ public/build/_shared/chunk-5GA4UCIN.js
@@ -0,0 +1,7 @@
+// public/img/social/youtube.png
+var youtube_default = "/build/_assets/youtube-6G4FYXU3.png";
+
+export {
+  youtube_default
+};
+//# sourceMappingURL=/build/_shared/chunk-5GA4UCIN.js.map
---
+++ public/build/routes/subscribe-2KVGN5OX.js
@@ -0,0 +1,162 @@
+import {
+  ja7_default
+} from "/build/_shared/chunk-DG7E5HKV.js";
+import {
+  youtube_default
+} from "/build/_shared/chunk-5GA4UCIN.js";
+import {
+  AltchaWrapper
+} from "/build/_shared/chunk-552RS725.js";
+import {
+  instagram_default,
+  linkedin_default,
+  x_default
+} from "/build/_shared/chunk-BPG2YPSP.js";
+import {
+  Link
+} from "/build/_shared/chunk-ROHSLC5J.js";
+import {
+  ja_default
+} from "/build/_shared/chunk-XIGSDZZU.js";
+import {
+  require_jsx_dev_runtime
+} from "/build/_shared/chunk-S4ZNHW4H.js";
+import {
+  __toESM
+} from "/build/_shared/chunk-IU43IUTG.js";
+
+// app/routes/subscribe.tsx
+var import_jsx_dev_runtime = __toESM(require_jsx_dev_runtime());
+function Index() {
+  return /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "subscribe-container", children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "header", children: [
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("img", { className: "logo", src: ja_default, alt: "The Poast Logo" }, void 0, false, {
+      fileName: "app/routes/subscribe.tsx",
+      lineNumber: 14,
+      columnNumber: 9
+    }, this),
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "outer-header", children: [
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "inner-header", children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "social", children: [
+        /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("a", { className: "x", href: "https://x.com/thepoast", target: "_blank", rel: "noopener noreferrer", children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("img", { src: x_default, alt: "X (Twitter)" }, void 0, false, {
+          fileName: "app/routes/subscribe.tsx",
+          lineNumber: 19,
+          columnNumber: 17
+        }, this) }, void 0, false, {
+          fileName: "app/routes/subscribe.tsx",
+          lineNumber: 18,
+          columnNumber: 17
+        }, this),
+        /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("a", { className: "ig", href: "https://instagram.com/thepoast", target: "_blank", rel: "noopener noreferrer", children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("img", { src: instagram_default, alt: "Instagram" }, void 0, false, {
+          fileName: "app/routes/subscribe.tsx",
+          lineNumber: 22,
+          columnNumber: 17
+        }, this) }, void 0, false, {
+          fileName: "app/routes/subscribe.tsx",
+          lineNumber: 21,
+          columnNumber: 17
+        }, this),
+        /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("a", { className: "li", href: "https://linkedin.com/company/thepoast", target: "_blank", rel: "noopener noreferrer", children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("img", { src: linkedin_default, alt: "LinkedIn" }, void 0, false, {
+          fileName: "app/routes/subscribe.tsx",
+          lineNumber: 25,
+          columnNumber: 17
+        }, this) }, void 0, false, {
+          fileName: "app/routes/subscribe.tsx",
+          lineNumber: 24,
+          columnNumber: 15
+        }, this),
+        /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("a", { className: "yt", href: "https://youtube.com/@thepoast", target: "_blank", rel: "noopener noreferrer", children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("img", { src: youtube_default, alt: "YouTube" }, void 0, false, {
+          fileName: "app/routes/subscribe.tsx",
+          lineNumber: 28,
+          columnNumber: 17
+        }, this) }, void 0, false, {
+          fileName: "app/routes/subscribe.tsx",
+          lineNumber: 27,
+          columnNumber: 15
+        }, this)
+      ] }, void 0, true, {
+        fileName: "app/routes/subscribe.tsx",
+        lineNumber: 17,
+        columnNumber: 13
+      }, this) }, void 0, false, {
+        fileName: "app/routes/subscribe.tsx",
+        lineNumber: 16,
+        columnNumber: 11
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "inner-header2", children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(Link, { to: "/", children: "Home" }, void 0, false, {
+        fileName: "app/routes/subscribe.tsx",
+        lineNumber: 33,
+        columnNumber: 14
+      }, this) }, void 0, false, {
+        fileName: "app/routes/subscribe.tsx",
+        lineNumber: 32,
+        columnNumber: 11
+      }, this)
+    ] }, void 0, true, {
+      fileName: "app/routes/subscribe.tsx",
+      lineNumber: 15,
+      columnNumber: 9
+    }, this),
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("form", { method: "post", action: "https://app.thepoast.com/subscription/form", children: [
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "input-wrapper", children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("input", { className: "email", type: "text", name: "firstName", placeholder: "First Name *" }, void 0, false, {
+        fileName: "app/routes/subscribe.tsx",
+        lineNumber: 38,
+        columnNumber: 13
+      }, this) }, void 0, false, {
+        fileName: "app/routes/subscribe.tsx",
+        lineNumber: 37,
+        columnNumber: 11
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "input-wrapper", children: [
+        /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("input", { className: "email", type: "email", name: "email", required: true, placeholder: "Email Address *" }, void 0, false, {
+          fileName: "app/routes/subscribe.tsx",
+          lineNumber: 41,
+          columnNumber: 13
+        }, this),
+        /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("button", { className: "submit", type: "submit", children: "Subscribe" }, void 0, false, {
+          fileName: "app/routes/subscribe.tsx",
+          lineNumber: 42,
+          columnNumber: 13
+        }, this)
+      ] }, void 0, true, {
+        fileName: "app/routes/subscribe.tsx",
+        lineNumber: 40,
+        columnNumber: 11
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(AltchaWrapper, {}, void 0, false, {
+        fileName: "app/routes/subscribe.tsx",
+        lineNumber: 44,
+        columnNumber: 11
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("input", { id: "6d48f", type: "hidden", name: "l", checked: true, value: "6d48fffe-7d37-4c14-b317-3e4cda33a647" }, void 0, false, {
+        fileName: "app/routes/subscribe.tsx",
+        lineNumber: 45,
+        columnNumber: 11
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("input", { type: "hidden", name: "nonce" }, void 0, false, {
+        fileName: "app/routes/subscribe.tsx",
+        lineNumber: 46,
+        columnNumber: 11
+      }, this)
+    ] }, void 0, true, {
+      fileName: "app/routes/subscribe.tsx",
+      lineNumber: 36,
+      columnNumber: 11
+    }, this),
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("img", { className: "headerimg", src: ja7_default, alt: "Instagram" }, void 0, false, {
+      fileName: "app/routes/subscribe.tsx",
+      lineNumber: 48,
+      columnNumber: 11
+    }, this)
+  ] }, void 0, true, {
+    fileName: "app/routes/subscribe.tsx",
+    lineNumber: 13,
+    columnNumber: 7
+  }, this) }, void 0, false, {
+    fileName: "app/routes/subscribe.tsx",
+    lineNumber: 12,
+    columnNumber: 5
+  }, this);
+}
+export {
+  Index as default
+};
+//# sourceMappingURL=/build/routes/subscribe-2KVGN5OX.js.map
