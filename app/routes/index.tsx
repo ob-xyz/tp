aead41c401b0291d314beba1a783304097e9ee2f
wn <45404661+ob-xyz@users.noreply.github.com>
2026-07-14 01:52:01 -0400

    1

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -90,7 +90,7 @@ export default function Index() {
         </Link>
         </div>
         <h1>This free 1-minute newsletter is your unfair advantage in a world moving faster every day</h1>
-        <h2>Every morning 40,000+ execs and builders scroll <a href="/today">The Poast</a> for technology's biggest stories and posts. Plus, the best AI tools, links, and ideas across the web. Bit snarky. All signal.</h2>  
+        <h2>Every morning 40,000+ execs and builders scroll <a href="/today">The Poast</a> for technology's biggest stories and posts. Plus, the best AI tools, links, and ideas from across the web. Bit snarky. All signal.</h2>  
           <div className="outer-header">
           <div className="inner-header">
             <div className="social">
