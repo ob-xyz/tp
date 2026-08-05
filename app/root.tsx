d1b0fe40b6afa74f8dff9feb8bb557929f04baa7
wn <45404661+ob-xyz@users.noreply.github.com>
2026-08-05 11:14:01 -0400

    11

--- app/root.tsx
+++ app/root.tsx
@@ -30,7 +30,7 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "The Poast",
-  description: "The Poast. Free newsletter. Never miss what everyone's talking about in tech.",
+  description: "The Poast. Free newsletter. Never miss what everyone's talking about.",
   viewport: "width=device-width,initial-scale=1"
 });
 
