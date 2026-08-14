6d391373324592426555d50ecd50dfcef3f8951d
wn <45404661+ob-xyz@users.noreply.github.com>
2026-08-14 14:27:08 -0400

    11

--- public/build/_shared/chunk-JOIKWTUL.js
+++
@@ -1,7 +0,0 @@
-// public/img/social/info.png
-var info_default = "/build/_assets/info-FQKLT5OI.png";
-
-export {
-  info_default
-};
-//# sourceMappingURL=/build/_shared/chunk-JOIKWTUL.js.map
--- public/build/_shared/chunk-XIGSDZZU.js
+++
@@ -1,7 +0,0 @@
-// public/img/ja.png
-var ja_default = "/build/_assets/ja-O6OFOOU2.png";
-
-export {
-  ja_default
-};
-//# sourceMappingURL=/build/_shared/chunk-XIGSDZZU.js.map
