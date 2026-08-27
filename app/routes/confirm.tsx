413f64e75fdc4d65c14907d4b19c91fccd0855b6
wn <45404661+ob-xyz@users.noreply.github.com>
2026-08-26 20:44:32 -0400

    123

--- app/routes/confirm.tsx
+++ app/routes/confirm.tsx
@@ -16,7 +16,7 @@ export default function Confirm() {
         <h1 style={{ fontSize: 52 }}>✓</h1>
         <h1 style={{ fontSize: 30, textAlign: "center" }}>Welcome back to The Poast</h1>
         <h2 style={{ fontSize: 18, textAlign: "left" }}><br />Thanks for giving us a second shot :)</h2>
-        <h2 style={{ fontSize: 18, textAlign: "left" }}><br />Expect our fast feed in your inbox every day. It stitches together the best business-minded news, posts, and and snarky comments from across the web. You can check out the latest issue <Link to='/live'>here →</Link></h2>
+       <h2 style={{ fontSize: 18, textAlign: "left" }}><br />Expect our fast feed in your inbox every day. It stitches together the best business-minded news, posts, and snarky comments from across the web. You can check out the latest issue <Link to='/live'>here →</Link></h2>
         <h2 style={{ fontSize: 18, textAlign: "left" }}><br /><em>P.S. If you don't receive an email, please check your spam or promotions folder and "move us" to your primary inbox to ensure you get The Poast each day.</em></h2>
         <h2 style={{ fontSize: 18, textAlign: "left" }}><br />See you soon!</h2>
         <h2 style={{ fontSize: 18, textAlign: "left" }}><br />—The Poast<br /><br /><br /><br /></h2>
