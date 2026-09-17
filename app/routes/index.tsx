ee9ba9192b5739602e84ebc4c8ff65007ef9c7d9
wn <45404661+ob-xyz@users.noreply.github.com>
2026-09-17 14:12:28 -0400

    123

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -570,7 +570,7 @@ export default function Index() {
             />
 
             <p>
-              Trusted by 15,000+ execs and builders
+              Trusted by 20,000+ execs and builders
             </p>
 
             <p>
@@ -659,7 +659,7 @@ export default function Index() {
         <h1>
           Scroll less. Know more.</h1>
         <h2>
-          Every day, 25,000+ execs and builders scroll
+          Every day, 20,000+ execs and builders scroll
           The Poast for a fast feed of interesting
           news, posts, and snarky comments.
         </h2>
