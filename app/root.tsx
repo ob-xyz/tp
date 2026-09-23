4e3b2b2b38730b00c8917bf3f6b0451675592618
wn <45404661+ob-xyz@users.noreply.github.com>
2026-09-23 14:15:11 -0400

    11

--- app/root.tsx
+++ app/root.tsx
@@ -29,7 +29,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "The Poast | The World's Fastest Feed",
+  title: "The Poast : The World's Fastest Feed",
   description: "Every day, 25,000+ execs and builders scroll The Poast for a fast feed of news, posts, and snarky comments.",
   viewport: "width=device-width,initial-scale=1"
 });
